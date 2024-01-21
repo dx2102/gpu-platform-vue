5a0bdb870afb12eaf43d7ecbd5b27a7be0af26c7
RickGao <rickgao6@outlook.com>
2024-01-21 16:58:43 -0500

    Add

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -25,11 +25,14 @@ function handle() {
 
 
 <template>
-  <div class="flex flex-col gap-4 p-4 h-full bg-gray-50">
-    <n-card title="Help" :segmented="true">
-    </n-card>
-
-  </div>
+  <main class="text-xl p-3 mt-16 flex flex-col items-center gap-2">
+    <div class="w-full max-w-3xl flex flex-col gap-1">
+      Help
+      <p class="text-2xl my-1">
+          For help please go to https://github.com/dx2102/gpu-platform-vue <br>
+      </p>
+    </div>
+  </main>
 </template>
 
 <style scoped>
